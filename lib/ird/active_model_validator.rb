613d5c97c3bbd44195595cc3b2fb0a3951d54afa
Kimmie <kimthu.bui@gmail.com>
2019-07-15 17:20:32 +0700

    Handle nil value

--- lib/ird/active_model_validator.rb
+++ lib/ird/active_model_validator.rb
@@ -4,7 +4,7 @@ module ActiveModel
   module Validations
     class IrdValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
-        return if IRD::Validator.validate(value)
+        return if IRD::Validator.validate(value.to_s)
         record.errors.add(attribute, options[:message] || :invalid)
       end
     end
